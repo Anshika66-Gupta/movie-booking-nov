1849b192b8e6713c43216a17cc499450d8dbd8c4
“Anshika <ansgupta.01@gmail.com>
2023-11-27 22:20:25 +0530

    27 Nov

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -17,4 +17,84 @@ const getAllUser = async (req, res, next) => {
     }
     return res.status(200).json({users})
 }
+const signUp = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    if(!name && name.trim() === "" && 
+        !email && email.trim() === "" &&  
+        !password && password.trim() === "") {
+        return res.status(400).json({ message: "Invalid Inputs"})
+    }
+    const hashedPassword = bcrypt.hashSync(password);
+    let users;
+try{
+    users  = new user({name, email, password: hashedPassword})
+    await users.save();
+} catch (err){
+    return res.json({err});
+}
 
+if(!users) {
+    return res.status(500).json({message: "Unexpected Error Occurred"});
+}
+
+return res.status(201).json({id:users._id})
+}
+
+const updateUser = async (req, res, next) => {
+    const id = req.params.id;
+    const { name, email, password } = req.body;
+    if(!name && name.trim() === "" && 
+        !email && email.trim() === "" &&  
+        !password && password.trim() === "") {
+        return res.status(422).json({ message: "Invalid Inputs"})
+    }
+
+    // const hashedPassword = bcrypt.hashSync(password);
+    let users;
+    try {
+        users = await user.findByIdAndUpdate(id, {name, email, password});
+    } catch (err){
+        return res.send(err.message);
+    }
+    if(!users){
+        return res.status(500).json({message: "Something went wrong"})
+    } 
+    res.status(200).json({message: "Updated successfully", user: users})
+}
+
+const deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+    let users;
+    try {
+        users = await user.findByIdAndDelete(id)
+    } catch (err){
+        return res.send(err.message);
+    }
+    if(!users){
+        return res.status(500).json({message: "Something went wrong"})
+    } 
+    res.status(200).json({message: "Deleted successfully", user: users})
+}
+
+const logIn = async (req, res, next) => {
+    const { email, password } = req.body;
+    if(!email && email.trim() === "" &&  
+        !password && password.trim() === "") {
+        return res.status(400).json({ message: "Invalid Inputs"})
+    }
+    let existingUser;
+    try {
+        existingUser = await user.findOne({ email });
+    } catch (err){
+        return res.send(err.message)
+    }
+    if(!existingUser){
+        return res.status(400).json({message: "User not found"})
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password)
+    if(!isPasswordCorrect) {
+        return res.status(400).json({message: "Incorrect Password"})
+    }
+    return res.status(200).json({message: "Login Successful", id:existingUser._id})
+}
--- backend/routes/admin-routes.js
+++ backend/routes/admin-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const adminRouter = express.Router;
 
+const {addAdmin} = require('../controllers/admin-controller');
+
 adminRouter.get('/signup', addAdmin);
 adminRouter.post('/login', adminLogin);
 adminRouter.get('/', getAdmin);
