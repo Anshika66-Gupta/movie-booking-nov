ebc93317980874d34634ef92bcbf0ffbc98b34fa
“Anshika <ansgupta.01@gmail.com>
2023-12-01 18:43:51 +0530

    1 dec

---
+++ movies/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AppBar, Box, Toolbar } from '@mui/material'
+import MovieIcon from '@mui/icons-material/Movie';
+function Header() {
+  return (
+      <AppBar>
+          <Toolbar>
+              <Box>
+                  
+              </Box>
+          </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
